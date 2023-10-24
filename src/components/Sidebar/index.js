5c8fb0be135d848639f64f3e8e088ffb3698b66b
veilchrome <keeita768900@gmail.com>
2023-10-24 16:08:25 +0700

    Projek React Selamet

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+import './index.scss'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to="/">
+            <img src={LogoS} alt="logo" />
+            <img src={LogoSubtitle} alt="selamet"/>
+        </Link>
+    </div>
+)
+
+export default Sidebar
