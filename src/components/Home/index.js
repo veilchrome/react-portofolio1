904418a78eaea37389c9a12947854780a4be624e
veilchrome <keeita768900@gmail.com>
2023-10-25 16:22:40 +0700

    Progress 40%

---
+++ src/components/Home/index.js
@@ -0,0 +1,23 @@
+import LogoTitle from '../../assets/images/logo-s.png';
+import { Link } from 'react-router-dom';
+import './index.scss'
+
+const Home = () => {
+
+    return (
+        <div className="container home-page">
+            <div className="text-zone">
+                <h1> Hi, <br />  I'm
+                <img src= { LogoTitle } alt="developer"/>
+                elamet
+                <br />
+                Web developer
+                </h1>
+                <h2>Front Developer / JavaScript</h2>
+                <Link to="/contact" className='flat-button'>CONTACT ME</Link>
+            </div>
+        </div>
+    );
+}
+
+export default Home
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,29 +1,109 @@
-import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
+import { useState } from 'react'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
-import {faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faWhatsapp,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+} from '@fortawesome/free-solid-svg-icons'
+import { Link, NavLink } from 'react-router-dom'
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to="/">
-            <img src={LogoS} alt="logo" />
-            <img className='sub-logo' src={LogoSubtitle} alt="selamet"/>
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon = {faHome} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon = {faUser} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon = {faEnvelope} color="#4d4d4e" />
-            </NavLink>
-            
-        </nav>
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false);
+
+  return (
+    <div className="nav-bar">
+      <Link 
+        className="logo"
+        to="/"
+        onClick={() => setShowNav(false)}>
+        <img src={LogoS} alt="Logo" />
+        <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
+      </Link>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink 
+          exact="true"
+          activeclassname="active"
+          to="/"
+          >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+        <NavLink 
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+          >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+        <NavLink
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+          
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href="https://www.linkedin.com/in/selamet-saputra-008938265/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://github.com/veilchrome"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.youtube.com/channel/UCZe7rv6ZPlWXOv8OzYGKgNA"
+            rel="noreferrer"
+            target="_blank"
+          >
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://wa.me/6281314217757" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faWhatsapp}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
     </div>
-)
+  )
+}
 
 export default Sidebar
